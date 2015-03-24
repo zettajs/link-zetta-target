270c9c7fccbc4e431f377c82423a831b4089e7ef
Matthew Dobson <mdobson4@gmail.com>
2015-03-23 22:02:38 -0400

    Adding usage addon and custom SQS collector.

--- target_server.js
+++ target_server.js
@@ -6,6 +6,10 @@ var DeviceDataSqs = require('zetta-device-data-sqs');
 
 var port = process.env.MAPPED_PORT || 3001;
 var version = process.env.VERSION || '0';
+var UsageApp = require('zetta-usage-addon');
+var UsageCollector = require('./sqs_collector');
+
+var app = new UsageApp()
 
 var options = {
   host: process.env.COREOS_PRIVATE_IPV4
@@ -22,6 +26,8 @@ var instance = zetta({
   peerRegistry: peerRegistry
 });
 
+instance.use(app.collect());
+
 instance.name('cloud-' + port);
 
 if (process.env.DEVICE_DATA_QUEUE) {
@@ -35,6 +41,15 @@ if (process.env.DEVICE_DATA_QUEUE) {
   instance.use(sqs.collect());
 }
 
+if (process.env.ZETTA_USAGE_QUEUE) {
+  var opts = {
+    queueUrl: process.env.ZETTA_USAGE_QUEUE,
+    accessKeyId: process.env.AWS_ACCESSKEY,
+    secretAccessKey: process.env.AWS_SECRET,
+    collector: app
+  }
+  Collector(opts);
+}
 
 instance.httpServer.server.on('close', function() {
   serviceRegistryClient.remove('cloud-target', instanceUrl);
